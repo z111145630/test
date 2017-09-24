dc060ec3b27954b2e27dcf29bbe861daf91366bb
yao <yao@gmail.com>
2017-09-24 22:15:33 +0800

    control1

---
+++ Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    // 消息列表
+	public function NewsList(Request $request){
+		$string ="1123";
+
+		return $string;
+	}
+}
